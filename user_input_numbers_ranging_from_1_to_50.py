c32f8ca4ff2c00afdcf8192de524ed0b3d15edf9
viviennebeduya <viviennebeduya@gmail.com>
2024-10-31 02:36:48 +0800

    user input

--- user_input_numbers_ranging_from_1_to_50.py
+++ user_input_numbers_ranging_from_1_to_50.py
@@ -1,4 +1,8 @@
 #ask user to input a number ranging from 1 to 50
+try:
+    user_input = int(input("Please input a number (from 1 to 50):"))
+except: 
+    print ("Make sure to input numbers only.")
 # ask again to input number
     #if valid, continue asking
     #if invalid, display how many inputted numbers are in the following range:
