b2f3b8f104de1c2cd8e9a01d306f039ef47b41fe
viviennebeduya <viviennebeduya@gmail.com>
2024-10-31 02:30:52 +0800

    draft

---
+++ user_input_numbers_ranging_from_1_to_50.py
@@ -0,0 +1,9 @@
+#ask user to input a number ranging from 1 to 50
+# ask again to input number
+    #if valid, continue asking
+    #if invalid, display how many inputted numbers are in the following range:
+        #1-10 = ?
+        #11-20 = ?
+        #21-30 = ?
+        #31-40 = ?
+        #41-50 = ?
