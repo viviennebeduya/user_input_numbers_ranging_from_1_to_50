be1687cd6410592955ff3e8b507a431503e86fe1
viviennebeduya <viviennebeduya@gmail.com>
2024-10-31 02:47:22 +0800

    loop iput

--- user_input_numbers_ranging_from_1_to_50.py
+++ user_input_numbers_ranging_from_1_to_50.py
@@ -1,8 +1,11 @@
+#initialize array for the ranges
+number_per_range = [0,0,0,0,0]
 #ask user to input a number ranging from 1 to 50
-try:
-    user_input = int(input("Please input a number (from 1 to 50):"))
-except: 
-    print ("Make sure to input numbers only.")
+while True:
+    try:
+        user_input = int(input("Please input a number (from 1 to 50):"))
+    except: 
+        print ("Make sure to input numbers only.")
 # ask again to input number
     #if valid, continue asking
     #if invalid, display how many inputted numbers are in the following range:
